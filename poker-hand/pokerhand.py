c56593b87ce0c32ee6d9ff798a76e66728fb966d
Brendon <localhost>
2021-04-24 19:18:50 +0200

    Added code to determine if hand is flush

--- poker-hand/pokerhand.py
+++ poker-hand/pokerhand.py
@@ -17,6 +17,15 @@ def score(hand):
         suit = card[-1]
         score[suit] += 1
     ranks = sorted(rcounts.items(),reverse=True)
+
+    # Determines if hand is a flush
+    if len(score) == 1:
+        print("Hand is a flush")
+        if ranks[0][0] == ranks[1][0]+1:
+            handBasicType = 'Straight Flush'
+        else:
+            handBasicType = 'Flush'
+        return score, handBasicType
     
 #score(['AS, 10C, 10H, 3D, 3S'])
 print(score(['AS, 10S, 9S, 3S, 4S']))
