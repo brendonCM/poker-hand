896fb68a4db3b83bbce6c54d09abdc3a3bfc3f5a
Brendon <localhost>
2021-04-24 17:30:24 +0200

    Additions made to include the suits of a hand

--- poker-hand/pokerhand.py
+++ poker-hand/pokerhand.py
@@ -2,15 +2,21 @@ from collections import defaultdict
 
 # Function to determine the number of occurences in one hand for each card
 def score(hand):
-    ranks = '2345678910JQKA'
-    cards = hand[0].split(',')
+    ranks = '23456789TJQKA'
+    handBasicType = 'n'
+    cards = ''.join(hand[0].split())
+    cards = cards.split(',')
     rcounts = defaultdict(int)
+    score = defaultdict(int)
     for card in cards:
         rank = card[:-1]
-        rcounts[rank] += 1
-    sortedranks = {k: v for k, v in sorted(rcounts.items(),key=lambda item: item[1],reverse=True)}
-
-       
-
-
-score(['AS, 10C, 10H, 3D, 3S'])
+        if rank.isalpha() == True:
+            rcounts[ranks.find(rank)+1]+=1
+        else:
+            rcounts[int(rank)] += 1
+        suit = card[-1]
+        score[suit] += 1
+    ranks = sorted(rcounts.items(),reverse=True)
+    
+#score(['AS, 10C, 10H, 3D, 3S'])
+print(score(['AS, 10S, 9S, 3S, 4S']))
